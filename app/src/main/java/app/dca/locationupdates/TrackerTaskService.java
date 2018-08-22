5966bbf922a3745acf26e6351dbcd435a3310849
hynra <hynra@yahoo.com>
2018-08-22 22:28:07 +0700

    add & register TrackerTaskService

---
+++ app/src/main/java/app/dca/locationupdates/TrackerTaskService.java
@@ -0,0 +1,24 @@
+package app.dca.locationupdates;
+
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GcmNetworkManager;
+import com.google.android.gms.gcm.GcmTaskService;
+import com.google.android.gms.gcm.TaskParams;
+
+public class TrackerTaskService extends GcmTaskService {
+
+    public static final String TAG = TrackerTaskService.class.getSimpleName();
+
+    @Override
+    public int onRunTask(TaskParams taskParams) {
+        Log.i(TAG, "onRunTask");
+        //Todo: remove null
+        Intent start = new Intent(this, null);
+        start.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(start);
+        return GcmNetworkManager.RESULT_SUCCESS;
+    }
+
+}
